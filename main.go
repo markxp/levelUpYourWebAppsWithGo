3700caa262bc8edbef7884b49f117ecaa1aba6fd
Bo-Yan, Cheng <markxp2002@gmail.com>
2017-01-16 10:09:26 +0800

    simple fileserver handler (removed later)

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main(){
+    mux := http.NewServeMux()
+    mux.Handle("/assets/",http.StripPrefix("/assets/",http.FileServer(http.Dir("assets/"))))
 
-    log.Fatal(http.ListenAndServe(":3000",http.FileServer(http.Dir("assets/"))))
+    log.Fatal(http.ListenAndServe(":3000",mux))
 }
