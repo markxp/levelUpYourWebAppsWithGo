f99cbebaec029f25b8c7c940f7e6a8aceaf0a344
Bo-Yan, Cheng <markxp2002@gmail.com>
2017-01-16 10:56:56 +0800

    Add `home` page to mux handler. Fix bugs by import required package.

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 func main(){
     mux := http.NewServeMux()
     mux.Handle("/assets/",http.StripPrefix("/assets/",http.FileServer(http.Dir("assets/"))))
-
+    mux.HandleFunc("/",homeRender)
     log.Fatal(http.ListenAndServe(":3000",mux))
+}
+
+func homeRender(w http.ResponseWriter, req *http.Request){
+    RenderTemplate(w, req, "index/home",nil)
 }
--- template.go
+++ template.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+    "net/http"
     "html/template"
+    "fmt"
 )
 
 var templates = template.Must(template.New("t").ParseGlob("templates/**/*.html"))
