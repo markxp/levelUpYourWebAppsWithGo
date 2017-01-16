91785a992c563479d8a2d1666590bac0ce44267f
Bo-Yan, Cheng <markxp2002@gmail.com>
2017-01-16 10:45:56 +0800

    add entry page and randering function

---
+++ template.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "html/template"
+)
+
+var templates = template.Must(template.New("t").ParseGlob("templates/**/*.html"))
+
+func RenderTemplate(w http.ResponseWriter, req *http.Request, name string, data interface{}){
+    err := templates.ExecuteTemplate(w, name, data)
+    if err != nil {
+        http.Error(w,fmt.Sprintf(errorTemplate,name,err),http.StatusInternalServerError)
+    }
+}
+
+var errorTemplate =`
+<html>
+    <body>
+        <h1>Error rendering template %s</h1>
+        <p>%s</p>
+    </body>
+</html>
+`
