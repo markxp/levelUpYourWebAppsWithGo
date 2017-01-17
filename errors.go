5fb85d396aaaeb918a34ef87776c1e074d0a0a36
Bo-Yan, Cheng <markxp2002@gmail.com>
2017-01-17 09:58:03 +0800

    add user validation error type and common errors

---
+++ errors.go
@@ -0,0 +1,19 @@
+package main
+
+import "errors"
+
+type ValidationError error
+
+var (
+    errNoUsername = ValidationError(errors.New("You must supply a username"))
+    errNoEmail = ValidationError(errors.New("Your must supply an E-mail address"))
+    errPasswardTooShort = ValidationError(errors.New("Your password is too short"))
+)
+
+
+// IsValidationError(error) (bool) uses "comma,ok" statement 
+// to return if error is an ValidationError
+func IsValidationError(err error) bool {
+    _, ok := err.(ValidationError)
+    return ok
+}
