5b838d9e0aec08e87e5b1b9365407c8273956e59
Bo-Yan, Cheng <markxp2002@gmail.com>
2017-01-17 17:28:45 +0800

    muti-error combination (exercise in chapter 5)

--- handlers.go
+++ handlers.go
@@ -1,52 +1,53 @@
 package main
 
 import (
-    "net/http"
-    "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
 )
 
-func AuthenticateRequest(w http.ResponseWriter, r *http.Request){
-    authenticated := false
-    if !authenticated {
-        http.Redirect(w, r, "/register", http.StatusFound)
-    }
+func AuthenticateRequest(w http.ResponseWriter, r *http.Request) {
+	authenticated := false
+	if !authenticated {
+		http.Redirect(w, r, "/register", http.StatusFound)
+	}
 }
 
-func HandleHome(w http.ResponseWriter, r *http.Request, params httprouter.Params){
-    RenderTemplate(w,r ,"index/home",nil)
+func HandleHome(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	RenderTemplate(w, r, "index/home", nil)
 }
 
-func HandleUserNew(w http.ResponseWriter, 
-    r *http.Request, 
-    _ httprouter.Params){
-        RenderTemplate(w,r,"user/new",nil)
+func HandleUserNew(w http.ResponseWriter,
+	r *http.Request,
+	_ httprouter.Params) {
+	RenderTemplate(w, r, "user/new", nil)
 }
 
-func HandleUserCreate(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
-    // retrive data from FormValue()
-    user, err := NewUser(r.FormValue("username"), r.FormValue("email"), r.FormValue("password"))
-    if err != nil {
-        if IsValidationError(err){
-            RenderTemplate(w, r, "user/new",map[string]interface{} {
-                    "Error": err.Error(),
-                    "User": user,
-            })
-            return
-        }
-        // panic if bcrypt went wrong
-        panic(err)
-    }
-    err = globalUserStore.Save(user)
-    if err != nil {
-        panic(err)
-    }
+func HandleUserCreate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	// retrive data from FormValue()
+	user, err := NewUser(r.FormValue("username"), r.FormValue("email"), r.FormValue("password"))
+	if err != nil {
+		if IsValidationError(err) {
+			RenderTemplate(w, r, "user/new", map[string]interface{}{
+				"Error": err.Error(),
+				"User":  user,
+			})
+			return
+		}
+		// panic if bcrypt went wrong
+		log.Fatalln(err.Error)
+		panic(err)
+	}
+	err = globalUserStore.Save(user)
+	if err != nil {
+		panic(err)
+	}
 
-    http.Redirect(w, r,"/?flash=User+Created", http.StatusFound)
+	http.Redirect(w, r, "/?flash=User+Created", http.StatusFound)
 }
-
 
 func HandleImageNew(w http.ResponseWriter,
-    r *http.Request,
-    params httprouter.Params){
-        //RenderTemplate
- }
+	r *http.Request,
+	params httprouter.Params) {
+	//RenderTemplate
+}
--- user.go
+++ user.go
@@ -1,57 +1,91 @@
 package main
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
 
-type User struct{
-    Username string
-    Email string
-    HashedPassword string
-    ID string
+type User struct {
+	Username       string
+	Email          string
+	HashedPassword string
+	ID             string
 }
 
 const (
-    passwordLength = 8
-    hashCost = 10
-    userIDLength  //why declare this?
+	passwordLength = 8
+	hashCost       = 10
+	userIDLength   //why declare this?
 )
 
-func NewUser(username, email, password string) (User, error){
-    user := User{
-        Email: email,
-        Username: username,
-    }
-    if username == "" {
-        return user, errNoUsername
-    }
-    if email == "" {
-        return user, errNoEmail
-    }
-    if password == ""{
-        return user, errNoPassword
-    }
-    if len(password) < passwordLength {
-        return user, errPasswardTooShort
-    }
+const (
+	errNoUsernameEnum       = 1 << iota
+	errNoEmailEnum          = 1 << iota
+	errNoPasswordEnum       = 1 << iota
+	errPasswardTooShortEnum = 1 << iota
+)
 
-    existingUser, err := globalUserStore.FindByUsername(username)
-    if err != nil {
-        return user, err
-    } else if existingUser != nil {
-        return user, errUsernameExists
-    }
+func NewUser(username, email, password string) (User, error) {
+	user := User{
+		Email:    email,
+		Username: username,
+	}
+	errorcode := 0
+	if username == "" {
+		errorcode += errNoUsernameEnum
+	}
+	if email == "" {
+		errorcode += errNoEmailEnum
+	}
+	if password == "" {
+		errorcode += errNoPasswordEnum
+	}
+	if len(password) < passwordLength {
+		errorcode += errPasswardTooShortEnum
+	}
 
-    existingUser, err = globalUserStore.FindByEmail(email)
-    if err != nil {
-        return user, err
-    } else if existingUser != nil {
-        return user, errEmailExists
-    }
+	if errorcode != 0 {
+		reterr := ValidationError(errors.New(""))
+		switch {
+		case errorcode-errPasswardTooShortEnum > 0:
+			reterr = ValidationError(errors.New(fmt.Sprintf("%s\n%s", errPasswardTooShort, reterr.Error())))
+			errorcode -= errPasswardTooShortEnum
+			fallthrough
+		case errorcode-errNoPasswordEnum > 0:
+			reterr = ValidationError(errors.New(fmt.Sprintf("%s\n%s", errNoPassword, reterr.Error())))
+			errorcode -= errNoPasswordEnum
+			fallthrough
+		case errorcode-errNoEmailEnum > 0:
+			reterr = ValidationError(errors.New(fmt.Sprintf("%s\n%s", errNoEmail, reterr.Error())))
+			errorcode -= errNoEmailEnum
+			fallthrough
+		case errorcode-errNoUsernameEnum > 0:
+			reterr = ValidationError(errors.New(fmt.Sprintf("%s\n%s", errNoUsername, reterr.Error())))
+			errorcode -= errNoUsernameEnum
+		}
+		return user, reterr
+	}
 
-    // If input is fine, hashed password and give it a ID.
-    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password),hashCost)
-    user.HashedPassword = string(hashedPassword)
+	existingUser, err := globalUserStore.FindByUsername(username)
+	if err != nil {
+		return user, err
+	} else if existingUser != nil {
+		return user, errUsernameExists
+	}
 
-    user.ID = GenerateID("user",userIDLength) 
+	existingUser, err = globalUserStore.FindByEmail(email)
+	if err != nil {
+		return user, err
+	} else if existingUser != nil {
+		return user, errEmailExists
+	}
 
-    return user, err //final err indicates bcrypt.GenerateFromPassword() went wrong
+	// If input is fine, hashed password and give it a ID.
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), hashCost)
+	user.HashedPassword = string(hashedPassword)
+
+	user.ID = GenerateID("user", userIDLength)
+
+	return user, err //final err indicates bcrypt.GenerateFromPassword() went wrong
 }
