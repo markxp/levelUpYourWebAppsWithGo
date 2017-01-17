a3a569b4c4c4ba08c3566d58220a244e94d38100
Bo-Yan, Cheng <markxp2002@gmail.com>
2017-01-17 16:04:17 +0800

    update error typing

--- errors.go
+++ errors.go
@@ -6,7 +6,7 @@ type ValidationError error
 
 var (
     errNoUsername       = ValidationError(errors.New("You must supply a username"))
-    errNoEmail          = ValidationError(errors.New("Your must supply an E-mail address"))
+    errNoEmail          = ValidationError(errors.New("You must supply an E-mail address"))
     errNoPassword       = ValidationError(errors.New("You must supply a password"))
     errPasswardTooShort = ValidationError(errors.New("Your password is too short"))
     errUsernameExists   = ValidationError(errors.New("The username is taken"))
