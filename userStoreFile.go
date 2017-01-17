18bbb9db9da8d2377c210e57247f9bd98de835c6
Bo-Yan, Cheng <markxp2002@gmail.com>
2017-01-17 12:17:42 +0800

    + File storage for userStore

---
+++ userStoreFile.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+    "encoding/json"
+    "strings"
+    "io/ioutil"
+)
+
+//First step - save to a file with JSON format
+type FileUserStore struct {
+    filename string
+    Users map[string]User
+}
+
+func (f FileUserStore)Save(user User) error {
+    f.Users[user.ID] = user
+    contents, err := json.MarshalIndent(f,"","  ")
+    if err != nil {
+        return err
+    } 
+
+    err = ioutil.WriteFile(f.filename, contents, 0660)
+    if err != nil {
+        return err
+    }
+}
+
+
+
+// In Find() function series, type FileUserStore,
+// error is not used yet. errors will be used to indicate storage/loading problem.
+// If user == nil, which means NOT FOUND. If error != nil, storage/loading breaks.
+func (f FileUserStore) Find(id string) (*User, error){
+    user, ok := f.Users[id]
+    if ok {
+        return &user, ok
+    }
+    return nil, nil
+}
+
+func (f FileUserStore) FindByUsername(username string) (*User, error){
+    // why filter empty email string? 
+    if username == "" {
+        return nil, nil
+    }
+
+    for _, v := range f.Users {
+        if strings.ToLower(username) == strings.ToLower(v.Username){
+            return &user, nil
+        }
+    }
+    return nil, nil
+}
+
+func (f FileUserStore) FindByEmail(email string) (*User, error){
+    // why filter empty email string? 
+    if email == "" {
+        return nil, nil
+    }
+
+    for _, v := range f.Users {
+        if strings.ToLower(email) == strings.ToLower(v.Email){
+            return &user, nil
+        }
+    }
+    return nil, nil
+}
+
+func NewFileUserStore(filename string) (*FileUserStore,error){
+    f := &FileUserStore {
+        Users: map[string]User {},
+        filename: filename,
+    }
+
+    contents, err := ioutil.ReadFile(filename)
+    if err != nil {
+        // file not exist is ok. 
+        if os.IsNotExist(err) {
+            // return a new *FileUserStore 
+            return f, nil 
+        }
+        return nil, err
+    }
+    err = json.Unmarshal(contents, f)
+    if err != nil {
+        return nil, err
+    }
+    return f, nil
+}
