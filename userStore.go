7b42a1e83308d7306144f39b374c82b06166e01b
Bo-Yan, Cheng <markxp2002@gmail.com>
2017-01-17 14:54:56 +0800

    In first version, + global user store initializes as a  JSON file.

--- userStore.go
+++ userStore.go
@@ -7,3 +7,15 @@ type UserStore interface {
     FindByUsername(string) (*User, error)
     Save(User) error
 }
+
+var globalUserStore UserStore
+
+// first version, use UserStoreFile
+func init(){
+    store, err := NewFileUserStore("./data/user.json")
+    if err != nil {
+        // add extra information and invoke panic()
+        panic(fmt.Errorf("Error creating user store: %s", err))
+    }
+    globalUserStore = store
+}
